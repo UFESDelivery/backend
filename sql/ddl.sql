a741989d6261c0bee31bc975c7f0f3346fe1e5af
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-04-30 00:36:19 -0300

    criação da tabela pedido

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -28,3 +28,17 @@ CREATE TABLE desconto (
         FOREIGN KEY (cd_cliente)
             REFERENCES cliente(cd_cliente)
 );
+
+
+CREATE TABLE pedido (
+    cd_pedido INTEGER NOT NULL AUTO_INCREMENT
+    , cd_cliente INTEGER NOT NULL
+    , cd_status INTEGER NOT NULL
+    , vl_total_impostos DOUBLE DEFAULT 0
+    , vl_total_compra DOUBLE NOT NULL
+    , vl_total_descontos DOUBLE DEFAULT 0
+    , vl_total_a_pagar DOUBLE NOT NULL
+    , dt_ultima_alteracao TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    , dt_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    , dt_fim TIMESTAMP DEFAULT NULL
+);
