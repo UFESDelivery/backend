b927976eb60925dbe0079be39bfa4eeda4a090cb
Davi GC <63275906+EmployingDGC@users.noreply.github.com>
2022-04-29 23:59:58 -0300

    criação da tabela desconto

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,3 +1,16 @@
 CREATE DATABASE ufesdelivery;
 
 USE ufesdelivery;
+
+CREATE TABLE desconto (
+    cd_desconto INTEGER NOT NULL AUTO_INCREMENT
+    , cd_cliente INTEGER
+    , ds_desconto VARCHAR(100) NOT NULL
+    , qt_usos INTEGER NOT NULL DEFAULT 0
+    , qt_max_usos INTEGER NOT NULL DEFAULT 1
+    , dt_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    , dt_fim TIMESTAMP DEFAULT NULL
+
+    , CONSTRAINT pk_desconto
+        PRIMARY KEY (cd_desconto)
+);
