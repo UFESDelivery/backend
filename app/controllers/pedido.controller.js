f4607e6229edccef63a5a4e12cb2c60cbde24080
Rafael de Andrade Prenholato <52474736+Rafa3lP@users.noreply.github.com>
2022-05-09 11:05:44 -0300

    correcao pedido e usuario

--- app/controllers/pedido.controller.js
+++ app/controllers/pedido.controller.js
@@ -1,27 +1,33 @@
 const db = require("../models");
-const Pedido = db.produto;
+const Pedido = db.pedido;
+const Usuario = db.usuario;
 const Op = db.Sequelize.Op;
 
-exports.criar = (req, res) => {
-     if (!req.body.cd_usuario) {
-        res.status(400).send({
-            message: "é necessário informar um usuario"
-        });
-        return;
-    }
+exports.criar = async (req, res) => {
+    try {
+        if (!req.body.cd_usuario) throw {
+            status: 400,
+            message: "é necessário informar um cliente"
+        }
 
-    const pedido = {
-        cd_usuario: req.body.cd_usuario,
-    };
- 
-    Pedido.create(pedido)
-        .then(data => {
-            res.send(data);
-        })
-        .catch(err => {
-            res.status(500).send({
-                message:
-                    err.message || "Algum erro ocorreu ao criar o pedido"
-            });
+        const dadosPedido = {
+            cd_usuario: req.body.cd_usuario,
+        };
+
+        const usuario = await Usuario.findOne({ 
+            where: { 
+                cd_usuario: dadosPedido.cd_usuario, 
+                cd_tipo_usuario: "Cliente",
+            } 
         });
+
+        if (!usuario) throw { status: 404, message: "Cliente não encontrado" };
+
+        const pedido = await Pedido.create(dadosPedido);
+
+        res.send(pedido);
+
+    } catch (err) {
+        return res.status(err.status || 500).send(err.message || err);
+    }
 };
